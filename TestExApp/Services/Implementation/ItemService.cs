942c61ea3d7aaeac878d562a0f97b32622d59be6
kamilkem <camilthdn@gmail.com>
2022-05-06 18:24:19 +0300

    логирование

--- TestExApp/Services/Implementation/ItemService.cs
+++ TestExApp/Services/Implementation/ItemService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,31 +13,49 @@ namespace TestExApp.Services.Implementation
     public class ItemService : IItemService
     {
         private readonly IRepository _repository;
-        public ItemService(IRepository repository)
+        private ILogger<ItemService> _logger;
+
+        public ItemService(IRepository repository, ILogger<ItemService> logger)
         {
             _repository = repository;
+            _logger = logger;
         }
 
         public async Task<ItemDto> GetAll(int pageSize, int pageNumber)
         {
-            var itemsCount = await _repository.GetItemsCount<Item>();
-            var items = await _repository.SelectAll<Item>()
-                .Skip(pageSize * (pageNumber-1))
-                .Take(5)
-                .ToListAsync();
-
-            return new ItemDto
+            try
             {
-                Items = items,
-                ItemsCount = itemsCount
-            };
+                var itemsCount = await _repository.GetItemsCount<Item>();
+                var items = await _repository.SelectAll<Item>()
+                    .Skip(pageSize * (pageNumber - 1))
+                    .Take(5)
+                    .ToListAsync();
+
+                return new ItemDto
+                {
+                    Items = items,
+                    ItemsCount = itemsCount
+                };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ошибка при получении списка объектов: {ex.Message }");
+                return new ItemDto();
+            }
         }
 
         public async Task SaveItems(List<Item> items)
         {
-            await _repository.DeleteAll<Item>();
+            try
+            {
+                await _repository.DeleteAll<Item>();
 
-            await _repository.SaveListAsync<Item>(items);
+                await _repository.SaveListAsync<Item>(items);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Ошибка при сохранении объектов: {ex.Message }");
+            }
         }
     }
 }
